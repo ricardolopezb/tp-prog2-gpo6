a9514966c9c85b33ae7b03f64f8f91dfe80c2a1b
Ricardo Lopez <ricardolopezbonaguro@gmail.com>
2020-10-25 16:33:20 -0300

    Se crearon los txt de sintomas

--- src/interfaz/Tester.java
+++ src/interfaz/Tester.java
@@ -1,13 +1,6 @@
 package interfaz;
 
-import archivos.Archivos;
-import entidades.Fecha;
-import usuarios.Admin;
-import usuarios.Ciudadano;
 import util.Check;
-import util.MetodosAuxiliares;
-
-import java.io.File;
 
 public class Tester {
     public static void main(String[] args) {
